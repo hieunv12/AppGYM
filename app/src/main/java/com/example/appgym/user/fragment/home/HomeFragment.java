b1887a056c1cd926bb221242fdaaab356f85eeb7
hieunv14 <hieunvmob@gmail.com>
2021-05-24 10:29:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appgym/user/fragment/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.appgym.user.fragment.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.appgym.R;
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
